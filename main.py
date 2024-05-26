afe03e7974b1f4a18e05aac345d7ef53628dd174
Id_Programming <156506190+idprogramming8@users.noreply.github.com>
2024-05-26 14:55:43 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from ursina import *
+from tkinter import filedialog
+app = Ursina()
+window.color = color.black
+file = input('Напишите названия текстуры планеты:')
+planet_model = Entity(model = 'sphere', texture = f'sprites/{file}.jpg')
+def update():
+    if held_keys['w']:
+        planet_model.z -= 4 * time.dt
+    if held_keys['s']:
+        planet_model.z += 4 * time.dt
+    if held_keys['a']:
+        planet_model.x += 4 * time.dt
+    if held_keys['d']:
+        planet_model.x -= 4 * time.dt
+    if held_keys['z']:
+        planet_model.rotation_y += 25 * time.dt
+    if held_keys['x']:
+        planet_model.rotation_y -= 25 * time.dt
+    if held_keys['y']:
+        planet_model.rotation_x += 25 * time.dt
+    if held_keys['h']:
+        planet_model.rotation_x -= 25 * time.dt
+app.run()
